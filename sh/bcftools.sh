04f03b9b782e6180e13c58a97c0f2dc7bfc970d7
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-02-12 11:13:53 +0000

    Added one more command; explain better the commands. Signed-off-by: Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>

--- sh/bcftools.sh
+++ sh/bcftools.sh
@@ -1,5 +1,20 @@
+# Notes
+
+# 1
+# -i can be used to filter on an expression (essentially any field)
+
+# -f applied to the FILTER field
+# -i used to find INFO/ANN field containg "|HIGH|"
 bcftools view -H \
 	-f PASS \ # Filter OPR >= 0.8
 	-i "ANN[*] ~ \"|HIGH|\"" \ # Filter if the INFO/ANN field contains a pattern
 	chr22_agg2_dedup_vep.vcf.gz \
+	| head
+	
+# -i used to filter on INFO/UK10KWGS_AC integer field being > 10
+bcftools view -H \
+	-f PASS \
+	chr22_agg2_dedup_vep.vcf.gz \
+	-r 22:16000001-217000000 \
+	-i "INFO/UK10KWGS_AC > 10" 
 	| head
